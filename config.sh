a1e7892611b7d388b1951c5bb5ede1c800db484f
ahirearyan2 <66762638+ahirearyan2@users.noreply.github.com>
2021-01-25 09:02:23 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-GITHUB_USER=Akianonymus
-GITHUB_EMAIL=anonymus.aki@gmail.com
+GITHUB_USER=ahirearyan2
+GITHUB_EMAIL=ahirearyan2016@gmail.com
 
-KBUILD_BUILD_USER="Aki"
+KBUILD_BUILD_USER="AryanAhire"
 KBUILD_BUILD_HOST="A_DEAD_PLANET"
 
-export oem=motorola
-export device=harpia
+export oem=samsung
+export device=On7pro
 
-ROM="PixelExperience"
-manifest_url="https://github.com/PixelExperience/manifest"
-export rom_vendor_name="aosp" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
-branch="pie"
+ROM="LineageOS"
+manifest_url="https://github.com/LineageOS/android"
+export rom_vendor_name="lineage" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
+branch="lineage-17.1"
 
-release_repo="Akianonymus/harpia_builds"
+release_repo="ahirearyan2/releases2"
 
 timezone="Asia/Kolkata"
