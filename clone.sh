02f1835ca33ad7f8dcf9699c0b6c719867544718
ahirearyan2 <66762638+ahirearyan2@users.noreply.github.com>
2021-01-25 09:07:47 +0530

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-# write commands if you want to clone some repos
+git clone https://github.com/Galaxy-MSM8916/android_device_samsung_msm8916-common device/samsung/msm8916-common
+
+git clone https://github.com/LineageOS/android_device_samsung_qcom-common -b lineage-17.1 device/samsung/qcom-common
+
+git clone https://github.com/LineageOS/android_hardware_samsung -b lineage-17.1 hardware/samsung
+
+git clone https://github.com/Galaxy-MSM8916/android_kernel_samsung_msm8916 kernel/samsung/msm8916
+
+git clone https://github.com/Galaxy-MSM8916/proprietary_vendor_samsung vendor/samsung
+
+git clone https://github.com/Galaxy-MSM8916/android_hardware_qcom_audio -b lineage-17.1-caf-msm8916 hardware/qcom-caf/msm8916/audio
+
+git clone https://github.com/LineageOS/android_external_sony_boringssl-compat -b lineage-17.1 external/sony/boringssl-compat
+
+git clone https://github.com/Galaxy-MSM8916/android-prebuilts-gcc-linux-x86-arm-arm-eabi-7.2 prebuilts/gcc/linux-x86/arm/arm-eabi-7.2
+
+git clone https://github.com/Galaxy-MSM8916/android_device_samsung_o7-common device/samsung/o7-common
+
+git clone https://github.com/Galaxy-MSM8916/android_device_samsung_o7prolte device/samsung/o7prolte
